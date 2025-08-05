6f503250689fd9b691edc06de18870da3a192aea
Michael Enry De Guzman <michaelydeguzman@gmail.com>
2025-08-05 00:45:33 -0700

    Added avatar placeholder in header bar
    Added avatar placeholder in header bar

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import DarkModeTrigger from "./dark-mode-trigger";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { SidebarTrigger } from "./ui/sidebar";
 
 export default function Header() {
   return (
-    <div className="w-full sticky top-0 h-14 flex justify-between  border-b-2 items-center px-3">
+    <div className="w-full sticky top-0 h-14 flex justify-between  border-b-2 items-center pr-6 pl-4">
       <SidebarTrigger />
-      <DarkModeTrigger />
+      <div className="flex items-center gap-3">
+        <DarkModeTrigger />
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src="https://avatars.githubusercontent.com/u/33743505?v=4" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div>Hi, Michael</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/dashboard/page.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface DashboardItem {
+  id: number;
+  component: React.ReactNode;
+  title: string;
+  description: string;
+  className?: string;
+}
+
+export default function Dashboard(): React.ReactNode {
+  const dashboardItems: DashboardItem[] = [
+    {
+      id: 1,
+      component: <></>,
+      title: "Expenses this Week",
+      description: "Summary of expenses for current week",
+    },
+    {
+      id: 2,
+      component: <></>,
+      title: "Expenses this Month",
+      description: "Summary of expenses for current month",
+    },
+    {
+      id: 3,
+      component: <></>,
+      title: "Expenses YTD",
+      description: "Summary of expenses for current year",
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="text-gray-500">De Guzman Household</div>
+        <h1 className="text-3xl font-semibold">Finance Dashboard</h1>
+      </div>
+
+      <div className="flex gap-4">
+        {dashboardItems.map((item) => (
+          <Card key={item.id} className="w-full flex">
+            <CardHeader>
+              <div className="flex flex-col gap-2">
+                <CardTitle>{item.title}</CardTitle>
+                <CardDescription> {item.description}</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div>{item.component}</div>
+            </CardContent>
+            <CardFooter>
+              <Button>See More</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+}
