04c0d98fdaf170fb254362c097676df504f0ada1
Michael Enry De Guzman <michaelydeguzman@gmail.com>
2025-08-27 18:32:46 -0700

    change to chevron

--- components/buttons/sortButton.tsx
+++ components/buttons/sortButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ArrowDownIcon, ArrowUpDownIcon, ArrowUpIcon } from "lucide-react";
+import { ChevronDown, ChevronsUpDown, ChevronUp } from "lucide-react";
 
 type SortButtonProps = {
   label: string;
@@ -13,11 +13,11 @@ export function SortButton({ label, sort, onToggle }: SortButtonProps) {
   const getSortIcon = () => {
     switch (sort) {
       case "asc":
-        return <ArrowUpIcon className="ml-2 h-4 w-4" />;
+        return <ChevronUp className="ml-2 h-4 w-4" />;
       case "desc":
-        return <ArrowDownIcon className="ml-2 h-4 w-4" />;
+        return <ChevronDown className="ml-2 h-4 w-4" />;
       default:
-        return <ArrowUpDownIcon className="ml-2 h-4 w-4" />;
+        return <ChevronsUpDown className="ml-2 h-4 w-4" />;
     }
   };
 
